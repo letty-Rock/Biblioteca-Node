37072d9ce7067a11e3660cc0a4a0d76477a36145
Letícia Roque Lima Oliveira <62360470+letty-Rock@users.noreply.github.com>
2022-02-08 11:06:46 -0300

    Início

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Irraa');
+const chalk = require('chalk');
+
+console.log(chalk.blue('Bem vindos'));
+
+const paragrafo = 'Texto retornado por uma função';
+function texto(string) {
+    return string;
+}
+console.log(texto(paragrafo));
